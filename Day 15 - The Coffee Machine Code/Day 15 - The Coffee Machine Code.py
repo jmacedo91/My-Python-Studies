b01ba1ec18031e9c8768db329824c6eba6c55f32
Jonathan Macedo <88295865+jmacedo91@users.noreply.github.com>
2022-07-19 21:44:57 -0300

    Repairing some bugs.

--- Day 15 - The Coffee Machine Code/Day 15 - The Coffee Machine Code.py
+++ Day 15 - The Coffee Machine Code/Day 15 - The Coffee Machine Code.py
@@ -1,10 +1,8 @@
 from data import MENU, resources
 
-is_turn_off = False
-money = 0
-
 
 def calculate_money():
+    """Returns the total calculated from coins inserted."""
     print("Please insert coins.")
     quarters = int(input("How many quarters?: "))
     dimes = int(input("How many dimes?: "))
@@ -15,35 +13,44 @@ def calculate_money():
 
 
 def report():
+    """Returns the total resources."""
     print(f"Water: {resources['water']}mL.")
     print(f"Milk: {resources['milk']}mL.")
     print(f"Coffee: {resources['coffee']}g.")
     print(f"Money: ${money}.")
 
 
-def check_resources(resource):
-    if MENU[coffee]["ingredients"][resource] > resources[resource]:
-        print(f"Sorry there is not enough {resource}.")
+def check_resources():
+    """Checks the resource stock."""
+    for resource in resources:
+        if MENU[coffee]["ingredients"][resource] > resources[resource]:
+            print(f"Sorry there is not enough {resource}.")
+            return False
+    return True
 
+
+is_turn_off = False
+money = 0
 
 while not is_turn_off:
     coffee = input("What would you like? (espresso/latte/cappuccino): ").lower()
     if coffee in MENU:
-        for resource in resources:
-            check_resources(resource)
-        total = calculate_money()
-        if total < MENU[coffee]["cost"]:
-            print("Sorry that's not enough money. Money refunded.")
-        else:
-            money += MENU[coffee]["cost"]
-            resources["water"] -= MENU[coffee]["ingredients"]["water"]
-            resources["milk"] -= MENU[coffee]["ingredients"]["milk"]
-            resources["coffee"] -= MENU[coffee]["ingredients"]["coffee"]
-            print(f"Here is your {coffee}. Enjoy!")
-            if total > MENU[coffee]["cost"]:
-                change = total - MENU[coffee]["cost"]
-                print(f"Here is ${round(change, 2)} dollars in change.")
+        if check_resources():
+            total = calculate_money()
+            if total < MENU[coffee]["cost"]:
+                print("Sorry that's not enough money. Money refunded.")
+            else:
+                money += MENU[coffee]["cost"]
+                resources["water"] -= MENU[coffee]["ingredients"]["water"]
+                resources["milk"] -= MENU[coffee]["ingredients"]["milk"]
+                resources["coffee"] -= MENU[coffee]["ingredients"]["coffee"]
+                print(f"Here is your {coffee} ☕. Enjoy!")
+                if total > MENU[coffee]["cost"]:
+                    change = total - MENU[coffee]["cost"]
+                    print(f"Here is ${round(change, 2)} dollars in change.")
     elif coffee == "report":
         report()
     elif coffee == "off":
         is_turn_off = True
+    else:
+        print("Sorry, invalid command.")
