96fbfd22b1264cf34305a2ad2a7abe7b33a8527f
Jonathan Macedo <88295865+jmacedo91@users.noreply.github.com>
2022-08-02 21:36:22 -0300

    Day 19 - Instances, State and Higher Order Func

---
+++ Day 19 - Instances, State and Higher Order Functions/Day 19 - Turtle-Race.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+all_turtles = list()
+x = -230
+y = -100
+
+for turtle in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle])
+    new_turtle.goto(x=x, y=y)
+    y += 40
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet.lower():
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+
+
+screen.exitonclick()
