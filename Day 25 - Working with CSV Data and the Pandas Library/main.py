63fe4d46c4412f88343c2c12957f5e3d835631c8
jmacedo91 <jonathan.silvamacedo@gmail.com>
2023-01-25 23:10:17 -0300

    DataFrames & Series

--- Day 25 - Working with CSV Data and the Pandas Library/main.py
+++ Day 25 - Working with CSV Data and the Pandas Library/main.py
@@ -1,7 +1,7 @@
 # with open("weather_data.csv") as data_file:
 #     data = data_file.readlines()
 #     print(data)
-
+#
 # import csv
 #
 # with open("weather_data.csv") as data_file:
@@ -15,4 +15,42 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+print(type(data))
+print(type(data["temp"]))
+
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+#
+# temp_mean = sum(temp_list) / len(temp_list)
+# print(temp_mean)
+#
+# print(data["temp"].mean())
+#
+# print(data["temp"].max())
+
+# Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+
+# Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+temp_fahrenheit = (data.temp * (9/5)) + 32
+print(data.temp)
+print(temp_fahrenheit)
+
+#Create a DataFrame from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
