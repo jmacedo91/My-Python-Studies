afdac26b6acff949c711c257c3bcb7dd78fca3a0
jmacedo91 <jonathan.silvamacedo@gmail.com>
2023-01-31 23:41:04 -0300

    Tkinter widgets

--- Day 27 - Tkinter, args, kwargs and Creating GUI Programs/main.py
+++ Day 27 - Tkinter, args, kwargs and Creating GUI Programs/main.py
@@ -1,12 +1,31 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")  # defaults to side = "top"
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()  # defaults to side = "top"
+
+# Update the properties
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+# Button
+def button_clicked():
+    print("I got clicked.")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+
+# Entry
+input = Entry(width=10)
+input.pack()
+print(input.get()) # Returns a String
 
 
 # while True:
--- Day 27 - Tkinter, args, kwargs and Creating GUI Programs/playground.py
+++ Day 27 - Tkinter, args, kwargs and Creating GUI Programs/playground.py
@@ -5,3 +5,27 @@ def add(*args):
 	return sum_numbers
 
 print(add(2, 3, 5, 8, 10))
+
+def calculate(n, **kwargs):
+	print(kwargs)
+	# for key, value in kwargs.items():
+	# 	print(key)
+	# 	print(value)
+	n += kwargs["add"]
+	n *= kwargs["multiply"]
+	print(n)
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+	def __init__(self, **kw):
+		self.make = kw.get("make") # use kw.get("make") then it will just return None and it won't give us an error
+		self.model = kw.get("model")
+		self.colour = kw.get("colour")
+		self.seats = kw.get("seats")
+
+my_car = Car(make="Nissan", model="Skyline", colour="black", seats=5)
+print(my_car.model)
+print(my_car.seats)
