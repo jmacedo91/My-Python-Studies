c34a8f1013fcc983f7bb3a75390d34c31d91513e
Jonathan Macedo <jonathan.silvamacedo@gmail.com>
2023-02-02 10:54:01 -0300

    Dynamic Typing Explained

--- Day 28 - Tkinter, Dynamic Typing and the Pomodoro GUI Application/main.py
+++ Day 28 - Tkinter, Dynamic Typing and the Pomodoro GUI Application/main.py
@@ -16,7 +16,7 @@ LONG_BREAK_MIN = 20
 
 
 def start_timer():
-    count_down(WORK_MIN * 60)
+    count_down(5 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
@@ -25,6 +25,9 @@ def count_down(count):
 
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+        
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     print(count)
