d3a90f02d343006468136c370da818f3a682e8e5
jmacedo91 <jonathan.silvamacedo@gmail.com>
2023-02-04 14:49:01 -0300

    Setting up the Pop-Ups and beginning the password generator

--- Day 29 - Building a Password Manager GUI App with Tkinter/main.py
+++ Day 29 - Building a Password Manager GUI App with Tkinter/main.py
@@ -1,24 +1,63 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+
+for char in range(nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(nr_symbols):
+    password_list += random.choice(symbols)
+
+for char in range(nr_numbers):
+    password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
 def save():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
 
-    messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail/Username: {username} "
-                                                  f"\nPassword: {password} "
-                                                  f"\nIs it ok to save?")
+    if website == "" or password == "":
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: "
+                                                              f"\nEmail/Username: {username}"
+                                                              f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", mode="a") as data:
+                data.write(f"{website} | {username} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
-    with open("data.txt", mode="a") as data:
-        data.write(f"{website} | {username} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=40, pady=40)
