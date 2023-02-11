a7003d4dcea0d70c81118b1bc2cd67b3eb4f7395
jmacedo91 <jonathan.silvamacedo@gmail.com>
2023-02-11 18:33:08 -0300

    Flash Card Project Finally Done

--- Day 31 - Flash Card App Capstone Project/main.py
+++ Day 31 - Flash Card App Capstone Project/main.py
@@ -6,24 +6,39 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 LOWER_FONT = ("Arial", 40, "italic")
 HIGHER_FONT = ("Arial", 60, "bold")
+current_card = {}
+data = None
 
 # --------------------- CREATE NEW FLASH CARDS ------------------------ #
-data = pd.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
-
-
-def flip_cards():
-	canvas.itemconfig(card, image=card_back_img)
-	canvas.itemconfig(card_title, text="English", fill="white")
-	canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+try:
+	data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+	data = pd.read_csv("data/french_words.csv")
+finally:
+	to_learn = data.to_dict(orient="records")
 
 
 def next_card():
+	global current_card, flip_timer
+	window.after_cancel(flip_timer)
 	current_card = random.choice(to_learn)
-	canvas.itemconfig(card_title, text="French")
-	canvas.itemconfig(card_word, text=current_card["French"])
-	window.after(3000, flip_cards)
+	canvas.itemconfig(card_title, text="French", fill="black")
+	canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+	canvas.itemconfig(card_background, image=card_front_img)
+	flip_timer = window.after(3000, flip_cards)
 
+
+def is_known():
+	to_learn.remove(current_card)
+	words_to_learn = pd.DataFrame(to_learn)
+	words_to_learn.to_csv("data/words_to_learn.csv", index=False)
+	next_card()
+
+
+def flip_cards():
+	canvas.itemconfig(card_title, text="English", fill="white")
+	canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+	canvas.itemconfig(card_background, image=card_back_img)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -33,11 +48,13 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, flip_cards)
+
 # Flash Card Canvas
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
-card = canvas.create_image(400, 263, image=card_front_img)
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="", font=LOWER_FONT)
 card_word = canvas.create_text(400, 263, text="", font=HIGHER_FONT)
 canvas.grid(row=0, column=0, columnspan=2)
@@ -49,7 +66,7 @@ right_img = PhotoImage(file="images/right.png")
 wrong_button = Button(image=wrong_img, command=next_card, highlightthickness=0)
 wrong_button.grid(row=1, column=0)
 
-right_button = Button(image=right_img, command=next_card, highlightthickness=0)
+right_button = Button(image=right_img, command=is_known, highlightthickness=0)
 right_button.grid(row=1, column=1)
 
 next_card()
