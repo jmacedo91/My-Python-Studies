b4c5788990fdb154e73a57ea7995d53c2a2e4b27
jmacedo91 <jonathan.silvamacedo@gmail.com>
2023-02-22 23:37:39 -0300

    Sending SMS via Twilio API

--- Day 35 - Keys, Authentication & Environment Variables - Send SMS/main.py
+++ Day 35 - Keys, Authentication & Environment Variables - Send SMS/main.py
@@ -1,12 +1,22 @@
 import requests
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
 
-API_KEY = "notthistime"
+load_dotenv("C:/Users/jhonn/AppData/Local/Programs/Python/Python311/EnvironmentVariables/.env.txt")
+
+api_key = os.getenv("OPEN_WEATHER_API_KEY")
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+my_phone_number = os.getenv("MY_PHONE_NUMBER")
+active_number = os.getenv("TWILIO_ACTIVE_NUMBER")
+
 forecast_3_hour_5_days = "https://api.openweathermap.org/data/2.5/forecast"
 
 weather_params = {
     "lat": -23.537205,
     "lon": -46.796230,
-    "appid": API_KEY,
+    "appid": api_key,
     "units": "metric"
 }
 
@@ -24,7 +34,14 @@ for hour_data in weather_slice:
         will_rain = True
 
 if will_rain:
-    print("Bring an umbrella.")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☂️",
+        from_=active_number,
+        to=my_phone_number
+    )
+    print(message.status)
 
 
 
